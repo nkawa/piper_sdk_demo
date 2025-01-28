cb13cde3f35dbf4b0ef6da66fea41d24fff9d48b
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2025-01-28 11:07:37 +0900

    Add English piper_slave_config.py

--- piper_slave_config.py
+++ piper_slave_config.py
@@ -3,6 +3,11 @@
 # 注意demo无法直接运行，需要pip安装sdk后才能运行
 # 设置机械臂为从动臂
 # 注意，如果是在机械臂处于主动臂模式下，发送设置指令后需要重新启动机械臂
+#
+# Note: The demo cannot run directly; the SDK must be installed via pip beforehand.
+# Set the robotic arm to follower mode.
+# Note: If the robotic arm is in active mode, you need to restart the robotic arm after sending the setting command.
+
 from typing import (
     Optional,
 )
@@ -10,6 +15,7 @@ import time
 from piper_sdk import *
 
 # 测试代码
+# Test Code
 if __name__ == "__main__":
     piper = C_PiperInterface()
     piper.ConnectPort()
